480988f948c367c10efe768cd78796e231b34f23
empty-works <pama.mark@gmail.com>
2021-02-20 17:18:25 +0100

    Implemented UpperLimitGenerator.

--- src/UpperLimitGenerator.java
+++ src/UpperLimitGenerator.java
@@ -1,5 +1,14 @@
 public class UpperLimitGenerator {
+    int level = 0;
     public UpperLimitGenerator(int level) {
-
+        this.level = level;
+    }
+    public int getUl() {
+        switch(level) {
+            case 1: return 10;
+            case 2: return 40;
+            case 3: return 100;
+            default: return 20;
+        }
     }
 }
