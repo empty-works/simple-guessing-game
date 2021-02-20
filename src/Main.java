35d7b2c5be3c9958bf3239b727fbcb27f1190481
empty-works <pama.mark@gmail.com>
2021-02-20 17:35:20 +0100

    Wrote out the pseudo code for the main method.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
-        // upperLimit =
+        // Ask user to select difficulty level with a menu
+        // level = user enters a corresponding number
+        // numberOfLives = NumberLivesGenerator(level)
+        // upperLimit = UpperLimitGenerator.getUpperLimit(level)
+        // answer = NumberGenerator.getAnswer(upperLimit)
+        // Ask user to guess number from 0 to upperLimit
+        // Let user enter number
+        // Tell user whether right or wrong. If right, congratulate. If wrong, try again.
+        // If trying again, let user enter another guess.
+        // Regardless, in the end calculate user's score.
     }
 }
