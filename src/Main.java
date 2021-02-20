350739ad9e949d41d0402a9bb44925c309568ebf
empty-works <pama.mark@gmail.com>
2021-02-20 18:11:04 +0100

    Created the Gameplay class and made some adjustments to the pseudo code.

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         // Ask user to select difficulty level with a menu
         // level = user enters a corresponding number
-        // numberOfLives = NumberLivesGenerator(level)
         // upperLimit = UpperLimitGenerator.getUpperLimit(level)
         // answer = NumberGenerator.getAnswer(upperLimit)
         // Ask user to guess number from 0 to upperLimit
         // Let user enter number
         // Tell user whether right or wrong. If right, congratulate. If wrong, try again.
         // If trying again, let user enter another guess.
+        // Show whether the user is above or below the guess. Also, show if close or not.
         // Regardless, in the end calculate user's score.
     }
 }
