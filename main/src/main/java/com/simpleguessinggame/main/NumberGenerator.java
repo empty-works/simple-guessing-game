ce84dc8bf181396f01cdd89486d518589dbde24c
empty-works <pama.mark@gmail.com>
2021-02-23 18:07:41 +0100

    Created unit test for get method in NumberGenerator.

--- main/src/main/java/com/simpleguessinggame/main/NumberGenerator.java
+++ main/src/main/java/com/simpleguessinggame/main/NumberGenerator.java
@@ -1,9 +1,7 @@
 package com.simpleguessinggame.main;
 
-import java.util.Random;
-
 public class NumberGenerator {
     public static int get(int upperLimit) {
-
+        return 0;
     }
 }
---
+++ main/src/test/java/com/simpleguessinggame/main/NumberGeneratorTest.java
@@ -0,0 +1,14 @@
+package com.simpleguessinggame.main;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberGeneratorTest {
+
+    @Test
+    void get() {
+        Assertions.assertEquals(1, 1);
+    }
+}
