6edccf5f033766932bcdc94a163222d9f4d47ddc
empty-works <pama.mark@gmail.com>
2021-02-24 16:10:27 +0100

    Implemented the lives score for the score generator class.

--- main/src/main/java/com/simpleguessinggame/main/generators/ScoreGenerator.java
+++ main/src/main/java/com/simpleguessinggame/main/generators/ScoreGenerator.java
@@ -2,6 +2,8 @@ package com.simpleguessinggame.main.generators;
 
 public class ScoreGenerator {
     public static int calculateScore(int answer, int userGuess, int numLives, int oldScore) {
+        int newScore = 0;
+        int proxScore = getProximityScore(answer, userGuess);
 
         return 0;
     }
@@ -16,7 +18,15 @@ public class ScoreGenerator {
             if(difference <= WARM) {
                 return 100;
             }
+            // User's guess is cold.
             else return 0;
         }
+        // User gets correct answer
+        else return 450;
+    }
+
+    private static int getLivesScore(int numLives) {
+        // More lives means a score accumulating with each guess.
+        return numLives * 5;
     }
 }
