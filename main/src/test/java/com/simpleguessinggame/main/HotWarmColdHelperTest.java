1ddde571e40e4954773f47f16b16e5ca7b4331b5
empty-works <pama.mark@gmail.com>
2021-02-26 18:04:54 +0100

    Created a test for the hot, warm, cold class.

---
+++ main/src/test/java/com/simpleguessinggame/main/HotWarmColdHelperTest.java
@@ -0,0 +1,16 @@
+package com.simpleguessinggame.main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotWarmColdHelperTest {
+
+    @Test
+    void getHotWarmColdScore() {
+    }
+
+    @Test
+    void getHotWarmColdMessage() {
+    }
+}
