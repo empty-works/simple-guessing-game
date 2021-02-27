be41a6d7c9d7ff7d36472ce9e9bd1e5a2216cade
empty-works <pama.mark@gmail.com>
2021-02-27 14:12:10 +0100

    Testing the hot score calculation.

--- main/src/test/java/com/simpleguessinggame/main/HotWarmColdHelperTest.java
+++ main/src/test/java/com/simpleguessinggame/main/HotWarmColdHelperTest.java
@@ -1,5 +1,6 @@
 package com.simpleguessinggame.main;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,11 @@ class HotWarmColdHelperTest {
 
     @Test
     void getHotWarmColdScore() {
+        HotWarmColdHelper help = new HotWarmColdHelper();
+        int hotScore = help.getHotWarmColdScore(20, 22);
+        int expectedHotScore = 300;
+        Assertions.assertEquals(expectedHotScore, hotScore);
+
     }
 
     @Test
