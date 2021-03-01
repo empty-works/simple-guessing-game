aa592b2f08fd23b8863d1e26b7e260fafee78c6f
empty-works <pama.mark@gmail.com>
2021-03-01 14:05:00 +0100

    Set a simple algo for determining number of lives depending on the difficulty level.

--- main/src/main/java/com/simpleguessinggame/main/gameplay/LivesHandler.java
+++ main/src/main/java/com/simpleguessinggame/main/gameplay/LivesHandler.java
@@ -1,6 +1,11 @@
 package com.simpleguessinggame.main.gameplay;
 
 public class LivesHandler {
-    public int numLives = 0;
-    public void setNumLives()
+    private int numLives = 0;
+    // Number of lives should be set depending on the difficulty level.
+    public void setNumLives(int level) {
+        // Multiply the level by a percent to linearly increase number of lives.
+        double numLivesDbl = level * 1.3;
+        numLives = (int) numLivesDbl;
+    }
 }
