de6ded6cb959b0da229029d6a7c746bf101e31ab
empty-works <pama.mark@gmail.com>
2021-03-02 17:01:46 +0100

    Added some getters and made a few adjustments to the naming.

--- main/src/main/java/com/simpleguessinggame/main/gameplay/GuessResultsContainer.java
+++ main/src/main/java/com/simpleguessinggame/main/gameplay/GuessResultsContainer.java
@@ -4,16 +4,36 @@ package com.simpleguessinggame.main.gameplay;
 public class GuessResultsContainer {
     private int counter = 0;
     private int[] guesses;
-    private int[] livesLeft;
+    private int livesLeft;
     private int[] scores;
+    private int maxNumLives = 0;
 
-    public GuessResultsContainer(int numLives) {
-        guesses = new int[numLives];
-        livesLeft = new int[numLives];
-        scores = new int[numLives];
+    public GuessResultsContainer(int maxNumLives) {
+        guesses = new int[maxNumLives];
+        livesLeft = maxNumLives;
+        scores = new int[maxNumLives];
+        this.maxNumLives = maxNumLives;
     }
 
-    public void addGuessLivesLeftScore(int guess, int livesLeft, int Score) {
+    public void addGuessLivesLeftScore(int guess, int livesLeft, int score) {
         guesses[counter] = guess;
+        this.livesLeft = livesLeft;
+        scores[counter] = score;
+    }
+
+    public int[] getGuesses() {
+        return guesses;
+    }
+
+    public int getLivesLeft() {
+        return livesLeft;
+    }
+
+    public int[] getScores() {
+        return scores;
+    }
+
+    public int getNumGuess() {
+        return maxNumLives - livesLeft;
     }
 }
