4c047169c620039350ffe720192c18cf7e3d4a2c
empty-works <pama.mark@gmail.com>
2021-03-02 16:37:44 +0100

    More arrays added to the guess container to track lives left and the running score. Instantiated all arrays with the maximum number of lives the user has.

--- main/src/main/java/com/simpleguessinggame/main/gameplay/GuessResultsContainer.java
+++ main/src/main/java/com/simpleguessinggame/main/gameplay/GuessResultsContainer.java
@@ -2,12 +2,15 @@ package com.simpleguessinggame.main.gameplay;
 
 // Stores all user guesses, number of lives left and scores
 public class GuessResultsContainer {
-    private int numLives = 0;
     private int counter = 0;
     private int[] guesses;
+    private int[] livesLeft;
+    private int[] scores;
 
     public GuessResultsContainer(int numLives) {
-        this.numLives = numLives;
+        guesses = new int[numLives];
+        livesLeft = new int[numLives];
+        scores = new int[numLives];
     }
 
     public void addGuessLivesLeftScore(int guess, int livesLeft, int Score) {
