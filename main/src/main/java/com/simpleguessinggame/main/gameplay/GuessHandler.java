b8dcb44f08c9ec97eb0b5b75904c9f6de69057fc
empty-works <pama.mark@gmail.com>
2021-03-02 18:03:18 +0100

    Refactored GuessHandler to make it clearer as to how it works.

--- main/src/main/java/com/simpleguessinggame/main/gameplay/GuessHandler.java
+++ main/src/main/java/com/simpleguessinggame/main/gameplay/GuessHandler.java
@@ -1,4 +1,5 @@
 package com.simpleguessinggame.main.gameplay;
+import com.simpleguessinggame.main.generators.LivesGenerator;
 import com.simpleguessinggame.main.generators.ScoreGenerator;
 
 import java.util.InputMismatchException;
@@ -6,30 +7,33 @@ import java.util.Scanner;
 
 public class GuessHandler {
     // Check if guess matches answer
-    public static GuessResultsContainer getResults(int answer) {
+    public static GuessResultsContainer getResults(int answer, int level) {
+
+        // TODO: Get number of lives from lives handler
+        // TODO: Figure out what to return to main from here
+        int maxNumLives = LivesGenerator.getNumLives(level);
+        return getResultsHelper(maxNumLives, answer);
+    }
+
+    private static GuessResultsContainer getResultsHelper(int maxNumLives, int answer) {
         int INVALID = -1;
         int currentScore = 0;
         Scanner scan = new Scanner(System.in);
         int userGuess = INVALID;
         boolean isCorrect = false;
-
-        // TODO: Get number of lives from lives handler
-        // TODO: Figure out what to return to main from here
-        int numLives = ;
-
-        private GuessResultsContainer getResults(int numLives) {
-            while(!isCorrect) {
-                try {
-                    userGuess = scan.nextInt();
-                    scan.nextLine();
-                    if(userGuess == answer) isCorrect = true;
-                    else {
-                        currentScore = ScoreGenerator.calculateScore(userGuess, answer, );
-                    }
+        GuessResultsContainer grc = new GuessResultsContainer(maxNumLives);
+        while(!isCorrect) {
+            try {
+                userGuess = scan.nextInt();
+                scan.nextLine();
+                if(userGuess == answer) isCorrect = true;
+                else {
+                    currentScore = ScoreGenerator.calculateScore(
+                            userGuess, answer, maxNumLives, currentScore);
                 }
-                catch(InputMismatchException ime) {
+            }
+            catch(InputMismatchException ime) {
 
-                }
             }
         }
     }
