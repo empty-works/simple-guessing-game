8a0562a58e76ce8dbbffba302838f49194189634
empty-works <pama.mark@gmail.com>
2021-03-03 14:32:26 +0100

    Refactored simple algorithm in GuessHandler.

--- main/src/main/java/com/simpleguessinggame/main/gameplay/GuessHandler.java
+++ main/src/main/java/com/simpleguessinggame/main/gameplay/GuessHandler.java
@@ -8,33 +8,31 @@ import java.util.Scanner;
 public class GuessHandler {
     // Check if guess matches answer
     public static GuessResultsContainer getResults(int answer, int level) {
-
-        // TODO: Get number of lives from lives handler
-        // TODO: Figure out what to return to main from here
         int maxNumLives = LivesGenerator.getNumLives(level);
         return getResultsHelper(maxNumLives, answer);
     }
 
     private static GuessResultsContainer getResultsHelper(int maxNumLives, int answer) {
-        int INVALID = -1;
         int currentScore = 0;
+        int numLives = maxNumLives;
         Scanner scan = new Scanner(System.in);
-        int userGuess = INVALID;
-        boolean isCorrect = false;
+        int userGuess;
+        boolean isDone = false;
         GuessResultsContainer grc = new GuessResultsContainer(maxNumLives);
-        while(!isCorrect) {
+        while(!isDone) {
             try {
                 userGuess = scan.nextInt();
                 scan.nextLine();
-                if(userGuess == answer) isCorrect = true;
-                else {
-                    currentScore = ScoreGenerator.calculateScore(
-                            userGuess, answer, maxNumLives, currentScore);
+                if(userGuess == answer || numLives == 0) {
+                    isDone = true;
                 }
+                currentScore = ScoreGenerator.calculateScore(
+                        userGuess, answer, numLives, currentScore);
             }
             catch(InputMismatchException ime) {
 
             }
         }
+        return grc;
     }
 }
